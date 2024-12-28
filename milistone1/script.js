e28ac831d8a988307503e42c6a92a6f45fc110da
mania ali <maniaa375@gmail.com>
2024-12-28 18:23:06 +0500

    Delete milistone1 directory

--- milistone1/script.js
+++
@@ -1,6 +0,0 @@
-var toggle = document.getElementById('toggle-skiils');
-var skills = document.getElementById('skiils');
-toggle.addEventListener('click', function () {
-    skills.style.display =
-        skills.style.display === 'none' ? 'block' : 'none';
-});
--- milistone1/script.ts
+++
@@ -1,7 +0,0 @@
-const toggleButton= document.getElementById('toggle-skiils') as HTMLButtonElement;
-const skill = document.getElementById('skiils') as HTMLDivElement;
-
-toggleButton.addEventListener('click', () => {
-  skill.style.display = 
-    skill.style.display === 'none' ? 'block' : 'none';
-});
